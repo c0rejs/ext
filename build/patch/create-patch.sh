56c3bc56e220f2df8c233527c9264ee352dbd225
zdm <zdm@softvisio.net>
2026-04-24 22:56:56 +0300

    refactor: update sh error trap

--- build/patch/create-patch.sh
+++ build/patch/create-patch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 set -Eeuo pipefail
-trap 'echo -e "⚠  Error ($0:$LINENO): $(sed -n "${LINENO}p" "$0" 2> /dev/null | grep -oE "\S.*\S|\S" || true)" >&2; return 3 2> /dev/null || exit 3' ERR
+trap 'echo "⚠  Error ($0:$LINENO): $BASH_COMMAND" && return 3 2> /dev/null || exit 3' ERR
 
 diff --recursive --no-ignore-file-name-case -u "../node_modules" "node_modules_patch" | grep -v "^Only in" > patch
