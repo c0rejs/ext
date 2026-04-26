790d9718b72e60832980198e1674f00a23db561b
zdm <zdm@softvisio.net>
2026-04-26 13:30:54 +0300

    fix: fix sh error trap

--- build/patch/create-patch.sh
+++ build/patch/create-patch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 set -Eeuo pipefail
-trap 'echo "⚠  Error ($0:$LINENO): $BASH_COMMAND" && return 3 2> /dev/null || exit 3' ERR
+trap 'echo "⚠  Error ($0:$LINENO, exit code: $?): $BASH_COMMAND" >&2' ERR
 
 diff --recursive --no-ignore-file-name-case -u "../node_modules" "node_modules_patch" | grep -v "^Only in" > patch
